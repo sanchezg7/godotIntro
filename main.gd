d180c0645837f2867dbd45a29f00b343ca5a434e
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-15 21:28:51 -0400

    Introducing the export concept

--- main.gd
+++ main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 const DANGER: int = 2
+@export var start_lives: int = 10
 var _lives: int = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_lives = start_lives
 	print(_lives)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
