61c3d3d781e5f979c8a2eee007740fec23c239a5
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-15 21:14:36 -0400

    Example of user input and changing variable

--- main.gd
+++ main.gd
@@ -1,22 +1,15 @@
 extends Node2D
 
 
+var _lives: int = 20
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello")
-	var score: int = 25
-	var name: String = "John"
-	var speed: float = 78.55464
-	var dead: bool = true
-	#var music_volume = null
-	print("score: ", score)
-
-	var fs: String = "speed: %.1f" % speed
-	print(fs)
-	var tmpl: String = "%s %s %.2f %s" % [name, score, speed, dead]
-	print(tmpl)
+	print(_lives)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print("delta:", delta)
+	if Input.is_action_just_pressed("ui_accept") == true:
+		_lives -= 1
+		print("Lives reduced: ", _lives)
 	pass
