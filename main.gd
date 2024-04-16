9b58b21e745646fd7b138915c9fa5b3fc9f9a9da
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-15 21:25:44 -0400

    refactoring to function

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-
-var _lives: int = 20
+const DANGER: int = 2
+var _lives: int = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +10,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept") == true:
-		_lives -= 1
-		print("Lives reduced: ", _lives)
+		reduce_lives()
 	pass
+
+func reduce_lives() -> void: 
+	_lives -=1
+	if _lives <= 0:
+		print("YOU DEAD BRUH")
+	elif _lives <= DANGER:
+		print("DANGER!:", _lives)
+	else:
+		print("OUUCH!:", _lives)
